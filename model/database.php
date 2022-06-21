485a99ab97ccd7dcd1081db5f9f14bd22c26b6a1
Isaac <isaacsaado69@gmail.com>
2022-06-21 16:36:50 -0400

    connect

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=s_system;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=remotemysql.com;dbname=ookqSHbDR6;charset=utf8', 'ookqSHbDR6', 'veKyTN02si');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
