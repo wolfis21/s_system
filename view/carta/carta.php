bb8b18c8481dde3c007825fcc488753c9256002a
Isaac <isaacsaado69@gmail.com>
2022-06-22 13:53:12 -0400

    fecha implementada

--- view/carta/carta.php
+++ view/carta/carta.php
@@ -1,107 +1,115 @@
-<?php 
+<?php
 // ob_start();
 ?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
-        <link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.css" />
-        <link rel="stylesheet" href="assets/css/style.css" />
-        
-        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+   <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
+   <link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.css" />
+   <link rel="stylesheet" href="assets/css/style.css" />
+
+   <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 
    <title>Carta de Entrega</title>
- </head>
- <body>
- <div class="container"> 
- <h1 class="page-header" align="Center"> CARTA DE ENTREGA</h1>
+</head>
 
-<?php foreach ($this->model2->Imprimir($rev->idRev_equipo) as $r) : ?>
-    <tr>
-       <h3 class="page-header">Tecnico</h3>
-        <div class="form-group">
-            <label>Cedula: </label>
-            <td><?php echo $r->Cedula; ?></td>
-        </div>
-        <div > 
-           <Label>Nombre: </Label>
-           <td><?php echo $r->pNombre." ".$r->pApellido; ?></td>
-        </div>
+<body>
+   <div class="container">
+      <h1 class="page-header" align="Center"> CARTA DE ENTREGA</h1>
+      <div id="current_date">
+         </p>
+         <script>
+            document.getElementById("current_date").innerHTML = Date();
+         </script>
 
-        <h3 class="page-header">Cliente</h3>
-        <div class="form-group"> 
-           <label>Cedula: </label>
-           <td><?php echo $r->idCedula; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-           <label>Nombre: </label>
-           <td><?php echo $r->Nombre." ".$r->Apellido; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-           <label> Direccion: </label>
-           <td><?php echo $r->Direccion; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-            <label>Telefono: </label>
-            <td><?php echo $r->Telefono; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-           <label>Correo: </label>
-           <td><?php echo $r->Correo; ?></td>
-        </div>
-        <h3 class="page-header" align="center">Equipo</h3>
-        <div class="form-group"> 
-           <label>Codigo de Equipo: </label>
-           <td><?php echo $r->idCodigo; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-           <label>Nombre del equipo: </label>
-           <td><?php echo $r->nombre_e; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-           <label>Fecha de ingreso: </label>
-           <td><?php echo $r->fecha_ingre; ?></td>
-        </div>
-          
-        <div class="form-group"> 
-           <label>Fecha de revision: </label>
-           <td><?php echo $r->fecha_rev; ?></td>
-        </div>
+         <?php foreach ($this->model2->Imprimir($rev->idRev_equipo) as $r) : ?>
+            <tr>
+               <h3 class="page-header">Tecnico</h3>
+               <div class="form-group">
+                  <label>Cedula: </label>
+                  <td><?php echo $r->Cedula; ?></td>
+               </div>
+               <div>
+                  <Label>Nombre: </Label>
+                  <td><?php echo $r->pNombre . " " . $r->pApellido; ?></td>
+               </div>
 
-        <div class="form-group"> 
-           <label>Descripcion de Revision: </label>
-           <td><?php echo $r->descrip_rev; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-           <label>Descripcion de Reemplazo: </label>
-           <td><?php echo $r->descrip_reemp; ?></td>
-        </div>
-        
-        <div class="form-group"> 
-            <label>Monto: </label>
-            <td><?php echo $r->presupuesto; ?></td>
-        </div>
+               <h3 class="page-header">Cliente</h3>
+               <div class="form-group">
+                  <label>Cedula: </label>
+                  <td><?php echo $r->idCedula; ?></td>
+               </div>
 
-    </tr>
-<?php endforeach; ?>
+               <div class="form-group">
+                  <label>Nombre: </label>
+                  <td><?php echo $r->Nombre . " " . $r->Apellido; ?></td>
+               </div>
 
-</div>
- </body>
- </html>
+               <div class="form-group">
+                  <label> Direccion: </label>
+                  <td><?php echo $r->Direccion; ?></td>
+               </div>
 
- <?php 
+               <div class="form-group">
+                  <label>Telefono: </label>
+                  <td><?php echo $r->Telefono; ?></td>
+               </div>
+
+               <div class="form-group">
+                  <label>Correo: </label>
+                  <td><?php echo $r->Correo; ?></td>
+               </div>
+               <h3 class="page-header" align="center">Equipo</h3>
+               <div class="form-group">
+                  <label>Codigo de Equipo: </label>
+                  <td><?php echo $r->idCodigo; ?></td>
+               </div>
+
+               <div class="form-group">
+                  <label>Nombre del equipo: </label>
+                  <td><?php echo $r->nombre_e; ?></td>
+               </div>
+
+               <div class="form-group">
+                  <label>Fecha de ingreso: </label>
+                  <td><?php echo $r->fecha_ingre; ?></td>
+               </div>
+
+               <div class="form-group">
+                  <label>Fecha de revision: </label>
+                  <td><?php echo $r->fecha_rev; ?></td>
+               </div>
+
+               <div class="form-group">
+                  <label>Descripcion de Revision: </label>
+                  <td><?php echo $r->descrip_rev; ?></td>
+               </div>
+
+               <div class="form-group">
+                  <label>Descripcion de Reemplazo: </label>
+                  <td><?php echo $r->descrip_reemp; ?></td>
+               </div>
+
+               <div class="form-group">
+                  <label>Monto: </label>
+                  <td><?php echo $r->presupuesto; ?></td>
+               </div>
+
+            </tr>
+         <?php endforeach; ?>
+
+      </div>
+</body>
+
+</html>
+
+<?php
 //  $html = ob_get_clean();
 //  echo $html;
 //    require_once './lib/dompdf/autoload.inc.php';
@@ -117,4 +125,4 @@
 //    $dompdf->render();
 //    //si ponemos true se nos descarga el pdf
 //    $dompdf->stream ("Carta de entrega.pdf", array("Attachment" => false));
- ?>
+?>
